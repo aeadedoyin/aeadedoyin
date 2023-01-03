ea1ef62bd1e19a6d58a8114132879426a927b232
Adedoyin Akande <aeadedoyin@gmail.com>
2023-01-03 01:24:13 +0100

    site update v2

---
+++ mouse/mouse.js
@@ -0,0 +1,46 @@
+const dot = document.querySelector('.dot')
+const dotOuter = document.querySelector('.dot-outer')
+const links = document.querySelectorAll('a')
+const audio = document.querySelector('.audio')
+
+audio.addEventListener('timeupdate', function (e) {
+  console.log(song.currentTime)
+})
+
+const toggleHover = (shouldEffect = false, isClick = false) => {
+  dot.classList.remove('dot-hover')
+  dotOuter.classList.remove('dot-outer-hover')
+  dotOuter.classList.remove('dot-outer-click')
+  if (shouldEffect) {
+    audio.play()
+    dot.classList.add('dot-hover')
+    dotOuter.classList.add('dot-outer-hover')
+  }
+  if (isClick) {
+    audio.play()
+    dotOuter.classList.add('dot-outer-click')
+  }
+}
+
+document.addEventListener('mousedown', function () {
+  toggleHover(true, true)
+})
+
+document.addEventListener('mouseup', function () {
+  toggleHover(false, false)
+})
+
+links.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    toggleHover(true, false)
+  })
+  item.addEventListener('mouseleave', () => {
+    toggleHover(false, false)
+  })
+})
+
+document.addEventListener('mousemove', function (e) {
+  const newPosition = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+  dot.style.transform = newPosition
+  dotOuter.style.transform = newPosition
+})
